0c8368db48b66e89272186963958bc986bc1fb72
0x-coder <214192707+0x-coder@users.noreply.github.com>
2025-10-22 12:14:12 -0700

    Added support for /supported endpoint

--- pkg/processor/processor.go
+++ pkg/processor/processor.go
@@ -80,9 +80,12 @@ func bootstrapFacilitatorClients(urls []string, cdpAPIKeyID, cdpAPIKeySecret str
 	// If CDP credentials are provided, only use CDP facilitator
 	if cdpAPIKeyID != "" && cdpAPIKeySecret != "" {
 		config := coinbasefacilitator.CreateFacilitatorConfig(cdpAPIKeyID, cdpAPIKeySecret)
-		client, supported := makeClient(config, httpClient)
-		for _, network := range supported {
-			networkToClients[network] = append(networkToClients[network], client)
+		config, err := CreateCdpAuthHeaders(config, cdpAPIKeyID, cdpAPIKeySecret)
+		if err == nil {
+			client, supported := makeClient(config, httpClient)
+			for _, network := range supported {
+				networkToClients[network] = append(networkToClients[network], client)
+			}
 		}
 	}
 
@@ -290,4 +293,40 @@ func Supported(c *facilitatorclient.FacilitatorClient) []string {
 	}
 
 	return networks
+}
+
+// CreateCdpAuthHeaders creates CDP auth headers
+// Based on https://github.com/coinbase/x402/blob/main/go/pkg/coinbasefacilitator/facilitator.go
+// Add support for /supported endpoint
+func CreateCdpAuthHeaders(config *x402types.FacilitatorConfig, cdpAPIKeyID, cdpAPIKeySecret string) (*x402types.FacilitatorConfig, error) {
+	config.CreateAuthHeaders = func() (map[string]map[string]string, error) {
+
+		verifyPath := fmt.Sprintf("%s/verify", config.URL)
+		settlePath := fmt.Sprintf("%s/settle", config.URL)
+		supportedPath := fmt.Sprintf("%s/supported", config.URL)
+
+		verifyToken, err := coinbasefacilitator.CreateAuthHeader(cdpAPIKeyID, cdpAPIKeySecret, config.URL, verifyPath)
+		if err != nil {
+			return nil, fmt.Errorf("failed to create verify auth header: %w", err)
+		}
+
+		settleToken, err := coinbasefacilitator.CreateAuthHeader(cdpAPIKeyID, cdpAPIKeySecret, config.URL, settlePath)
+		if err != nil {
+			return nil, fmt.Errorf("failed to create settle auth header: %w", err)
+		}
+
+		supportedToken, err := coinbasefacilitator.CreateAuthHeader(cdpAPIKeyID, cdpAPIKeySecret, config.URL, supportedPath)
+		if err != nil {
+			return nil, fmt.Errorf("failed to create supported auth header: %w", err)
+		}
+
+		correlationHeader := coinbasefacilitator.CreateCorrelationHeader()
+
+		return map[string]map[string]string{
+			"verify":    {"Authorization": verifyToken, "Correlation-Context": correlationHeader},
+			"settle":    {"Authorization": settleToken, "Correlation-Context": correlationHeader},
+			"supported": {"Authorization": supportedToken, "Correlation-Context": correlationHeader},
+		}, nil
+	}
+	return config, nil
 }
